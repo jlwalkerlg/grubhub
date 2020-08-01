44a1bcffe02a9ce38a6b363ca0437b927a1a0bf0
Jordan Walker <walker.jlg@gmail.com>
2020-08-01 09:10:47 +0100

    tailwind config

--- client/tailwind.config.js
+++ client/tailwind.config.js
@@ -1,27 +1,33 @@
+const defaults = require("tailwindcss/defaultTheme");
+
+const green = {
+  100: "#dbf5db",
+  200: "#b7ebb7",
+  300: "#94e292",
+  400: "#70d86e",
+  500: "#4cce4a",
+  600: "#44b943",
+  700: "#3da53b",
+  800: "#2e7c2c",
+  900: "#173e16",
+};
+
 module.exports = {
   purge: ["./views/**/*.tsx", "./components/**/*.tsx"],
   theme: {
-    container: (theme) => ({
+    container: {
       center: true,
-      padding: theme("spacing.4"),
-    }),
+      padding: defaults.spacing["4"],
+    },
     extend: {
       colors: {
         primary: {
-          default: "#c53030",
-          darker: "#9b2c2c",
+          ...defaults.colors.red,
+          default: defaults.colors.red["700"],
+          darker: defaults.colors.red["800"],
         },
-        green: {
-          100: "#dbf5db",
-          200: "#b7ebb7",
-          300: "#94e292",
-          400: "#70d86e",
-          500: "#4cce4a",
-          600: "#44b943",
-          700: "#3da53b",
-          800: "#2e7c2c",
-          900: "#173e16",
-        },
+        secondary: green,
+        green: green,
       },
     },
   },
