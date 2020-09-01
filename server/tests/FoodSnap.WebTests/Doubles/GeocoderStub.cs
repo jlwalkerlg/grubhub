dfde04c3b70ec77fc299020f082437e2036cc0fd
Jordan Walker <walker.jlg@gmail.com>
2020-09-01 17:46:20 +0100

    Override Autofac registrations in web app test factory

--- server/tests/FoodSnap.WebTests/Doubles/GeocoderStub.cs
+++ server/tests/FoodSnap.WebTests/Doubles/GeocoderStub.cs
@@ -6,7 +6,6 @@ using FoodSnap.Domain.Restaurants;
 
 namespace FoodSnap.WebTests.Doubles
 {
-    // TODO: register this in web functional tests instead of real service
     public class GeocoderStub : IGeocoder
     {
         public Task<Result<Coordinates>> GetCoordinates(Address address)
--- server/tests/FoodSnap.WebTests/WebAppFactory.cs
+++ server/tests/FoodSnap.WebTests/WebAppFactory.cs
@@ -1,6 +1,11 @@
+using System;
 using System.Linq;
+using Autofac;
+using Autofac.Extensions.DependencyInjection;
+using FoodSnap.Application.Services.Geocoding;
 using FoodSnap.Infrastructure.Persistence.EF;
 using FoodSnap.Web;
+using FoodSnap.WebTests.Doubles;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.Mvc.Testing;
 using Microsoft.EntityFrameworkCore;
@@ -12,6 +17,12 @@ namespace FoodSnap.WebTests
 {
     public class WebAppFactory : WebApplicationFactory<Startup>
     {
+        protected override IHost CreateHost(IHostBuilder builder)
+        {
+            builder.UseServiceProviderFactory(new TestServiceProviderFactory());
+            return base.CreateHost(builder);
+        }
+
         protected override void ConfigureWebHost(IWebHostBuilder builder)
         {
             builder.ConfigureServices((webHostBuilderContext, services) =>
@@ -35,6 +46,32 @@ namespace FoodSnap.WebTests
                 services.AddDbContext<AppDbContext>(options =>
                     options.UseInMemoryDatabase("TestDB"));
             });
+        }
+    }
+
+    public class TestServiceProviderFactory : IServiceProviderFactory<ContainerBuilder>
+    {
+        public ContainerBuilder CreateBuilder(IServiceCollection services)
+        {
+            var builder = new ContainerBuilder();
+            builder.Populate(services);
+
+            return builder;
+        }
+
+        public IServiceProvider CreateServiceProvider(ContainerBuilder builder)
+        {
+            ConfigureContainer(builder);
+
+            return new AutofacServiceProvider(builder.Build());
+        }
+
+        private static void ConfigureContainer(ContainerBuilder builder)
+        {
+            builder
+                .RegisterType<GeocoderStub>()
+                .As<IGeocoder>()
+                .SingleInstance();
         }
     }
 
