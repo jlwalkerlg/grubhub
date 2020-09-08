4fa7867fe6f39efb132885a95994bf42c6b6ca83
Jordan Walker <walker.jlg@gmail.com>
2020-09-08 16:01:36 +0100

    Initial dashboard

---
+++ client/src/components/Icons/DashboardIcon.tsx
@@ -0,0 +1,20 @@
+import React, { FC } from "react";
+
+interface Props {
+  className?: string;
+}
+
+const DashboardIcon: FC<Props> = ({ className }) => {
+  return (
+    <svg
+      className={className}
+      xmlns="http://www.w3.org/2000/svg"
+      viewBox="0 0 24 24"
+    >
+      <path d="M0 0h24v24H0z" fill="none" />
+      <path d="M3 13h8V3H3v10zm0 8h8v-6H3v6zm10 0h8V11h-8v10zm0-18v6h8V3h-8z" />
+    </svg>
+  );
+};
+
+export default DashboardIcon;
