09cd188b08f9b75baab6c98a7e0250a6c34330d9
Jordan Walker <walker.jlg@gmail.com>
2020-09-09 15:05:38 +0100

    Server side auth on all pages

--- client/src/pages/_app.tsx
+++ client/src/pages/_app.tsx
@@ -1,11 +1,20 @@
 import { AppProps } from "next/app";
+import { NextPageContext } from "next";
+import { AppContextType } from "next/dist/next-server/lib/utils";
 
 import { Provider } from "react-redux";
 import { useStore } from "../store/store";
 
+import Axios from "axios";
+import cookie from "cookie";
+import { GetAuthUserResponse } from "~/api/AuthApi";
+import { UserDto } from "~/api/dtos/UserDto";
+import { initializeStore, State } from "~/store/store";
+import { UserRole } from "~/store/auth/User";
+
 import "~/styles/index.css";
 
-function App({ Component, pageProps }: AppProps) {
+export default function App({ Component, pageProps }: AppProps) {
   const store = useStore(pageProps.initialReduxState);
 
   return (
@@ -15,4 +24,61 @@ function App({ Component, pageProps }: AppProps) {
   );
 }
 
-export default App;
+const getUser = async (context: NextPageContext): Promise<UserDto> => {
+  const token = cookie.parse(context.req.headers.cookie || "")["auth_token"];
+  if (!token) return null;
+
+  try {
+    const response = await Axios.get<GetAuthUserResponse>(
+      `${process.env.NEXT_PUBLIC_API_BASE_URL}/auth/user`,
+      {
+        headers: {
+          Cookie: `auth_token=${token}`,
+        },
+        withCredentials: true,
+      }
+    );
+
+    return response.data.data;
+  } catch (e) {
+    context.res.setHeader(
+      "Set-Cookie",
+      cookie.serialize("auth_token", "", {
+        expires: new Date(0),
+      })
+    );
+
+    return null;
+  }
+};
+
+App.getInitialProps = async (context: AppContextType) => {
+  // don't run on client
+  if (context.ctx.req === undefined) {
+    return {
+      pageProps: {
+        initialReduxState: null,
+      },
+    };
+  }
+
+  const user = await getUser(context.ctx);
+
+  let state: State = null;
+  if (user !== null) {
+    const store = initializeStore();
+    state = store.getState();
+    state.auth.user = {
+      id: user.id,
+      name: user.name,
+      email: user.email,
+      role: UserRole[user.role],
+    };
+  }
+
+  return {
+    pageProps: {
+      initialReduxState: state,
+    },
+  };
+};
