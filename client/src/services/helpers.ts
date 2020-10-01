7cb0fe0086742beb95f3fc6706c15b1637f3b268
jlwalkerlg <walker.jlg@gmail.com>
2020-10-01 16:16:38 +0100

    Script loader return if script already loaded

--- client/src/services/helpers.ts
+++ client/src/services/helpers.ts
@@ -10,6 +10,11 @@ export const redirect = (res: ServerResponse, location: string) => {
 
 export const loadScript = (src: string): Promise<void> => {
   return new Promise((resolve) => {
+    if (document.querySelector(`script[src="${src}"]`) !== null) {
+      resolve();
+      return;
+    }
+
     const script = document.createElement("script");
     script.src = src;
     script.addEventListener("load", () => resolve());
