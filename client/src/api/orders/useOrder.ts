9a07c56c53f3468916ef85bb11e688c3b9beb536
Jordan Walker <walker.jlg@gmail.com>
2021-02-12 15:31:36 +0000

    useOrder no longer overrides enabled config

--- client/src/api/orders/useOrder.ts
+++ client/src/api/orders/useOrder.ts
@@ -1,6 +1,5 @@
-import { QueryConfig, useQuery, useQueryCache } from "react-query";
+import { QueryConfig, useQuery } from "react-query";
 import Api, { ApiError } from "../Api";
-import useAuth from "../users/useAuth";
 import { OrderDto } from "./OrderDto";
 
 export function getOrderQueryKey(orderId: string) {
@@ -9,21 +8,14 @@ export function getOrderQueryKey(orderId: string) {
 
 export default function useOrder(
   orderId: string,
-  config: QueryConfig<OrderDto, ApiError> = {}
+  config?: QueryConfig<OrderDto, ApiError>
 ) {
-  const { isLoggedIn } = useAuth();
-
-  const cache = useQueryCache();
-
   return useQuery<OrderDto, ApiError>(
     getOrderQueryKey(orderId),
     async () => {
       const response = await Api.get<OrderDto>(`/orders/${orderId}`);
       return response.data;
     },
-    {
-      ...config,
-      enabled: isLoggedIn && (config.enabled ?? true),
-    }
+    config
   );
 }
