98689ec8fa8349c9c881b147c9183e87b0d80e5e
Jordan Walker <jordan@microworld.co.uk>
2021-05-11 12:41:08 +0100

    Add DoogalGeocodingTests.cs

---
+++ server/tests/WebTests/Services/Geocoding/DoogalGeocodingTests.cs
@@ -0,0 +1,22 @@
+﻿using System.Threading.Tasks;
+using Shouldly;
+using Web.Services.Geocoding;
+using Xunit;
+
+namespace WebTests.Services.Geocoding
+{
+    public class DoogalGeocodingTests
+    {
+        private readonly DoogalGeocoder geocoder = new();
+
+        [Fact]
+        public async Task LookupCoordinates_Finds_Coordinates()
+        {
+            var result = await geocoder.LookupCoordinates("BD18 1LT");
+
+            result.ShouldBeSuccessful();
+            result.Value.Latitude.ShouldNotBe(default);
+            result.Value.Longitude.ShouldNotBe(default);
+        }
+    }
+}
